64b2a349c6bd2a21f5de8b7b68b94d38b26909b6
unknown <C:\Users\\AppData\Roaming\The Bat!>
2021-10-20 21:53:58 +0300

    finishcalc

--- calculator/script.js
+++ calculator/script.js
@@ -1,5 +1,24 @@
-conts calc = document.querySelector ('calc')
+const calc = document.querySelector('.calc');
+const result = document.querySelector('#result');
 
 calc.addEventListener('click', function(event) {
-    console.log (event.target);
+if(!event.target.classList.contains('calc__btn')) return; 
+// проверка таргет события на наличие calc__btn
+
+    const value = event.target.innerText; 
+    //получение содержимого кнопки
+    switch  (value) {
+        case 'C':
+            result.innerText = '';
+            break;
+
+        case '=':
+            result.innerText = eval(result.innerText).toFixed(2);
+            break;
+
+            default:
+            result.innerText += value;
+    }
+    
+
 });
